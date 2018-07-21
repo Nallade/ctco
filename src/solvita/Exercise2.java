b00605e5d3dc48f116456f6fd10727c992c16362
Solvita <solvita.v@gmail.com>
2018-07-21 16:13:42 +0300

    prints user input in even/odd numbers

--- src/solvita/Exercise2.java
+++ src/solvita/Exercise2.java
@@ -6,8 +6,17 @@ public class Exercise2 {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         System.out.println("Enter number: ");
-        Integer baseLength = reader.nextInt();
+        int baseLength = reader.nextInt();
         reader.close();
-        System.out.println(baseLength);
+
+        if (baseLength % 2 == 0) {
+            for (int x = 2; x <= baseLength; x = x + 2) {
+                System.out.println(x);
+            }
+        } else {
+            for (int x = 1; x <= baseLength; x = x + 2) {
+                System.out.println(x);
+            }
+        }
     }
 }

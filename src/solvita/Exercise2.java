b2e0a69a84818d82218b7206a4cce8e421d19724
Solvita <solvita.v@gmail.com>
2018-07-21 23:05:12 +0300

    method printStars

--- src/solvita/Exercise2.java
+++ src/solvita/Exercise2.java
@@ -3,24 +3,26 @@ package solvita;
 import java.util.Scanner;
 
 public class Exercise2 {
+    private static void printStars(int count) {
+        for (int i = 1; i <= count; i++) {
+            System.out.print("*");
+        }
+    }
+
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        System.out.println("Enter number: ");
+        System.out.print("Base's length: ");
         int baseLength = reader.nextInt();
         reader.close();
 
         if (baseLength % 2 == 0) {
-            for (int x = 2; x <= baseLength; x = x + 2) {
-                for (int n = 1; n <= x; n++) {
-                    System.out.print("*");
-                }
+            for (int nStars = 2; nStars <= baseLength; nStars = nStars + 2) {
+                printStars(nStars);
                 System.out.println();
             }
         } else {
-            for (int x = 1; x <= baseLength; x = x + 2) {
-                for (int n = 1; n <= x; n++) {
-                    System.out.print("*");
-                }
+            for (int nStars = 1; nStars <= baseLength; nStars = nStars + 2) {
+                printStars(nStars);
                 System.out.println();
             }
         }

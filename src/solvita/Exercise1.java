7c72ed68e09f965fc319905043bac08ed82ad8e3
Solvita <solvita.v@gmail.com>
2018-07-22 20:52:08 +0300

    final version

--- src/solvita/Exercise1.java
+++ src/solvita/Exercise1.java
@@ -4,25 +4,24 @@ public class Exercise1 {
     public static int solution(int[] array) {
         int countNumbers = 1;
         int max = Integer.MIN_VALUE;
+        int result = 0;
         for (int i = 0; i < array.length - 1; i++) {
             if (array[i] == array[i + 1]) {
                 countNumbers = countNumbers + 1;
                 if (countNumbers > max) {
                     max = countNumbers;
+                    result = array[i];
                 }
-                System.out.println(i + "-th element (" + array[i] + ") is equal to " + (i + 1) + "-th element  (" + array[i + 1] + ") and we have " + countNumbers + " numbers");
             } else {
                 countNumbers = 1;
-                System.out.println(i + "-th element (" + array[i] + ") is NOT equal to " + (i + 1) + "-th element (" + array[i + 1] + ") and we had " + countNumbers + " numbers");
-
             }
         }
-        return max;
+        return result;
     }
 
     public static void main(String[] args) {
         int[] intArray = new int[]{1, 5, 5, 5, 6, 6, 7, 7, 7, 7, 7, 7, 7, 7, 5, 5, 5, 5, 5};
         int result = solution(intArray);
-        System.out.print(result);
+        System.out.println(result);
     }
 }
